900b6bde174715f803c3d24e46d20d392fb7a0fc
Yuri Paiva <yuriqpaiva@gmail.com>
2022-11-13 14:52:13 -0300

    Add jest config

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create Category Use Case", () => {
+  it("should sum correctly", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("2 + 2 should not be 5", () => {
+    const soma = 2 + 2;
+    const result = 5;
+
+    expect(soma).not.toBe(result);
+  });
+});
