801c31a7ec768fa674650aee5066dfea3e871bb5
Yuri <yuriqpaiva@gmail.com>
2022-11-23 21:28:17 -0300

    Add Rental creation and tests

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,8 +1,14 @@
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 interface IRentalsRepository {
   findOpenRentalByCar: (car_id: string) => Promise<Rental | undefined>;
   findOpenRentalByUser: (user_id: string) => Promise<Rental | undefined>;
+  create: ({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO) => Promise<Rental>;
 }
 
 export { IRentalsRepository };
