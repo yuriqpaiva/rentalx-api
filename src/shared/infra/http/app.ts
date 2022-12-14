137607c51ecd688c881cc2a90b2b3dc5e07efe7c
Yuri <yuriqpaiva@gmail.com>
2022-12-14 19:53:13 -0300

    Correct cars express static images name

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
-app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
