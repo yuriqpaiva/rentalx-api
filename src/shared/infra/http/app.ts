6e290c456533ac91f4d6c7294d1081d26e937736
Yuri <yuriqpaiva@gmail.com>
2022-12-18 12:52:52 -0300

    Add Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -17,6 +18,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
